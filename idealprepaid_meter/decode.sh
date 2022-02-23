62822c156e049c0133720ebce6ac543bebe3284c
Anirudh Ramesh <ramesh.anirudh95@gmail.com>
2022-02-23 17:41:58 +0530

    Edit idealprepaid_meter decode.sh

--- idealprepaid_meter/decode.sh
+++ idealprepaid_meter/decode.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 echo "code,meterNumber,receiptNumber,transactionAmount,valueAmount,feeAmount,taxAmount,electricity,timestamp" > /destination/meter.csv
+
 for file in /source/*.eml; do
+
 	content=`tail "$file" | sed '/[?:]/d' | tr -d '\r\n' | base64 -d | sed -e 's/Thank you for using Ideal Prepaid!//g' -e 's/Product Details://g' -e 's/Meter:/,/g' -e 's/RCPT#/,/g' -e 's/Tender:/,/g' -e 's/Amt:/,/g' -e 's/*Fee:/,/g' -e 's/*Fees Taxes:/,/g' -e 's/U:/,/g' -e 's/Tender in INR//g' -e 's/T(s)//g' -e 's/kWh//g' -e '/Paid/d' | tr -d '\r\n' | sed -e 's/ ----------------------------------------//g' -e 's/----------------------------------------/\n/g'`
+
+#	content=`tail "$file" | sed '/[?:]/d' | tr -d '\r\n' | base64 --decode | sed -e 's/Thank you for using Ideal Prepaid!//g' -e 's/Product Details://g' -e 's/Meter:/,/g' -e 's/RCPT#/,/g' -e 's/Tender:/,/g' -e 's/Amt:/,/g' -e 's/*Fee:/,/g' -e 's/*Fees Taxes:/,/g' -e 's/U:/,/g' -e 's/Tender in INR//g' -e 's/T(s)//g' -e 's/kWh//g' -e '/Paid/d' | tr -d '\r\n' | sed -e 's/ ----------------------------------------//g' -e 's/----------------------------------------/\n/g'`
+
 	filename=`echo $file | sed -e 's/\/source\/Product Delivery - _No-Reply @ Prepaid_ _noreply@trustprepaid.com_ - //g' -e 's/.eml//g'`
+
+#	filename=`echo $file | sed -e 's/.\/Product Delivery - "No-Reply @ Prepaid" <noreply@trustprepaid.com> - //g' -e 's/.eml//g'`
+
 	echo $content,$filename >> /destination/meter.csv
+
 done
